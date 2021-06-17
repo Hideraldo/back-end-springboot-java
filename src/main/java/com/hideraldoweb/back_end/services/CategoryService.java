0dfb50c042e9a9469296fd16b9484bc7f88f8a22
Hideraldo Luis Rondon <hideraldorondon@gmail.com>
2021-06-17 15:07:20 -0300

    Final project

--- src/main/java/com/hideraldoweb/back_end/services/CategoryService.java
+++ src/main/java/com/hideraldoweb/back_end/services/CategoryService.java
@@ -3,11 +3,17 @@ package com.hideraldoweb.back_end.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.hideraldoweb.back_end.entities.Category;
 import com.hideraldoweb.back_end.repositories.CategoryRepository;
+import com.hideraldoweb.back_end.services.exceptions.DatabaseException;
+import com.hideraldoweb.back_end.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -22,5 +28,33 @@ public class CategoryService {
 	public Category findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		return obj.get();
+	}
+	
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Category update(Long id, Category obj) {
+		try {
+			Category entity = repository.getById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());
 	}
 }
--- src/main/java/com/hideraldoweb/back_end/services/OrderService.java
+++ src/main/java/com/hideraldoweb/back_end/services/OrderService.java
@@ -3,11 +3,17 @@ package com.hideraldoweb.back_end.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.hideraldoweb.back_end.entities.Order;
 import com.hideraldoweb.back_end.repositories.OrderRepository;
+import com.hideraldoweb.back_end.services.exceptions.DatabaseException;
+import com.hideraldoweb.back_end.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class OrderService {
@@ -22,5 +28,36 @@ public class OrderService {
 	public Order findById(Long id) {
 		Optional<Order> obj = repository.findById(id);
 		return obj.get();
+	}
+	
+	public Order insert(Order obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Order update(Long id, Order obj) {
+		try {
+			Order entity = repository.getById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Order entity, Order obj) {
+		entity.setMoment(obj.getMoment());
+		entity.setOrderStatus(obj.getOrderStatus());
+		entity.setClient(obj.getClient());
+		entity.setPayment(obj.getPayment());
 	}
 }
